f8100e24e98743c9648d47156bc40110785979a1
David Moina <davemolina777@gmail.com>
2021-09-25 14:56:17 -0500

    Add serializer attributes

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer
   include JSONAPI::Serializer
-  attributes 
+  attributes :name, :brew_method_id, :temperature, :water, :coffee_bean_id, :grind, :time, :filter, :bloom_time
 end
