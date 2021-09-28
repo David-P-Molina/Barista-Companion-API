cecb6e61c1515915dfc9c49d166cdd13bce165c4
David Moina <davemolina777@gmail.com>
2021-09-28 14:19:56 -0500

    Add custom name setter for roaster to coffeebean

--- app/models/coffee_bean.rb
+++ app/models/coffee_bean.rb
@@ -1,3 +1,11 @@
 class CoffeeBean < ApplicationRecord
   belongs_to :roaster
+
+  #name helper
+  def roaster_name=(name)
+    self.roaster = Roaster.find_or_create_by(name: name)
+  end
+  def roaster_name
+    self.roaster.try(:name)
+  end
 end
