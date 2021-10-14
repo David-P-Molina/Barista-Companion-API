549f0743fb0989a5d0a088ee44c96bb8fb526f9a
David Moina <davemolina777@gmail.com>
2021-10-14 12:48:44 -0500

    ADd custom validation for roaster year

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -2,4 +2,11 @@ class Roaster < ApplicationRecord
     validates :name, :description, presence: true
     validates :name, uniqueness: true
     validates :year_founded, numericality: true
+    validate :year_founded_is_not_future_date
+
+    #custom validation
+    def year_founded_is_not_future_date
+        if :year_founded.present? && :year_founded > Date.today.year
+            errors.add(:year_founded, "Roaster Cannot be set in future!")
+    end
 end
