f3e73e243555e5b160f11832b20fe54b7ce9d8dd
David Moina <davemolina777@gmail.com>
2021-10-15 12:31:17 -0500

    Remove username references

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,12 +3,6 @@ class Recipe < ApplicationRecord
   belongs_to :coffee_beans
 
   #name helper
-  def username=(name)
-    self.user = User.find_or_create_by(username: name)
-  end
-  def username
-    self.roaster.try(:username)
-  end
   def brew_method_name=(name)
     self.brew_method = BrewMethod.find_or_create_by(name: name)
   end
--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -7,7 +7,7 @@ class Roaster < ApplicationRecord
 
     #custom validation
     def year_founded_is_not_future_date
-        if :year_founded.present? && :year_founded > Date.today.year
+        if :year_founded.present? && 'year_founded'.to_i > (Date.today.year + 1)
             errors.add(:year_founded, "Roaster Cannot be set in future!")
         end
     end
