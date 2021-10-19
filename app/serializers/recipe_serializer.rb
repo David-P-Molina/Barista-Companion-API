50a89b6677618195e92561deebf57638bead6bfa
David Moina <davemolina777@gmail.com>
2021-10-19 17:10:12 -0500

    Reorganize attributes order and add notes and roast_date

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,5 @@
 class RecipeSerializer
   include JSONAPI::Serializer
-  attributes :name, :brew_method_id, :brew_method_name, :temperature, :water_in_grams, :coffee_in_grams, :coffee_bean_id, :coffee_bean_name, :grind, :time, :filter, :bloom_time, :roaster_name
+  attributes :name, :date_attempted, :brew_method_id, :brew_method_name, :coffee_bean_id, :coffee_bean_name, :roaster_name, :roast_date, :grind, :coffee_in_grams, :water_in_grams, :temperature, :filter, :time, :bloom_time, :notes
   set_key_transform :camel_lower 
 end
