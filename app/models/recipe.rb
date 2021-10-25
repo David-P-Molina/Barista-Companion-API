b8769efc9794b7dd095be1816d9a0c2f3f9c6df9
David Moina <davemolina777@gmail.com>
2021-10-25 16:47:27 -0500

    Fix string to numbers

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ require 'byebug'
 class Recipe < ApplicationRecord
   belongs_to :brew_method
   belongs_to :coffee_bean
-  validates :name, :brew_method_id, :temperature, :time, :filter, :name, :coffee_in_grams, :brew_method_id, :grind, presence: true
+  validates :name, :brew_method_id, :temperature, :time, :name, :coffee_in_grams, :brew_method_id, :grind, presence: true
   validates :temperature, :water_in_grams, :bloom_time, :time, numericality: { only_integer: true }
   validate :temperature_cannot_exceed_boiling
   validate :roast_date_cannot_be_after_brew_date
