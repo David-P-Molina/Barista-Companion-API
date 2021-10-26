bbef712bd43ec1261bb9941548670592a8953a4d
David Moina <davemolina777@gmail.com>
2021-10-25 20:21:03 -0500

    Add ID attributes

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,5 @@
 class RecipeSerializer
   include JSONAPI::Serializer
-  attributes :name, :date_attempted, :brew_method_id, :brew_method_name, :coffee_bean_id, :coffee_bean_name, :roaster_name, :roast_date, :grind, :coffee_in_grams, :water_in_grams, :temperature, :filter, :time, :bloom_time, :notes, :deletable
+  attributes :name, :id, :date_attempted, :brew_method_id, :brew_method_name, :coffee_bean_id, :coffee_bean_name, :roaster_name, :roast_date, :grind, :coffee_in_grams, :water_in_grams, :temperature, :filter, :time, :bloom_time, :notes, :deletable
   set_key_transform :camel_lower 
 end
