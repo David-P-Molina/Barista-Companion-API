86556c8275fbf7cd6e4a89fabacf943257ee800d
David Moina <davemolina777@gmail.com>
2021-10-26 10:35:15 -0500

    Fix relationships to delete correct items

--- app/models/brew_method.rb
+++ app/models/brew_method.rb
@@ -1,5 +1,5 @@
 class BrewMethod < ApplicationRecord
-    has_many :coffee_beans
+    has_many :recipes, dependent: :delete_all
     validates :name, :description, :equipment, presence: true
     validates :name, uniqueness: true
 end
